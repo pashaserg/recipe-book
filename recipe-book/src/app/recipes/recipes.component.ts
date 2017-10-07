92d4bd983763c14ad105d8c60b86910fd2bef6fb
pashaserg <pashaserg@gmail.com>
2017-10-07 17:27:11 +0300

    Now we use Observable at serives

--- recipe-book/src/app/recipes/recipes.component.ts
+++ recipe-book/src/app/recipes/recipes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import {Recipe} from './recipe.model';
 import {RecipeService} from './recipe.service';
+
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
@@ -9,17 +9,10 @@ import {RecipeService} from './recipe.service';
   providers: [ RecipeService]
 })
 export class RecipesComponent implements OnInit {
-selectedRecipe: Recipe;
 
-  constructor( private recipeService: RecipeService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.recipeService.recipeSelected
-      .subscribe(
-        (recipe: Recipe) => {
-          this.selectedRecipe = recipe;
-        }
-      );
   }
 
 }
