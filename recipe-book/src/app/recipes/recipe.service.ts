ab07ef804c68705a255d923b5dc890effc581ecd
pashaserg <pashaserg@gmail.com>
2017-10-13 01:51:36 +0300

    now we use forms with validation. we can delete, edit, add recipes and ingredients, and add some ingredients at shopping list

--- recipe-book/src/app/recipes/recipe.service.ts
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -1,26 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+
 import { Recipe } from './recipe.model';
-import { Injectable} from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is test',
-      'http://www.tellusaboutus.com/comments/images/BK-WebComment/BB_WHOPPER-v1.png', [
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
         new Ingredient('Meat', 1),
         new Ingredient('French Fries', 20)
       ]),
-    new Recipe('Another Test Recipe',
-      'This is test',
-      'https://truffle-assets.imgix.net/bd5f9ea8-ho-emily-615-chicken-schnitzel-thumbnail-16x9-v01.png', [
+    new Recipe('Big Fat Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+      [
         new Ingredient('Buns', 2),
         new Ingredient('Meat', 1)
-      ]
-    )
+      ])
   ];
 
   constructor(private slService: ShoppingListService) {}
@@ -35,5 +39,20 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
--- recipe-book/src/app/shopping-list/shopping-list.service.ts
+++ recipe-book/src/app/shopping-list/shopping-list.service.ts
@@ -3,7 +3,7 @@ import { Subject } from 'rxjs/Subject';
 
 export class ShoppingListService {
 ingredientsChanged = new Subject<Ingredient[]>();
-
+startedEditing = new Subject<number>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
@@ -11,6 +11,10 @@ ingredientsChanged = new Subject<Ingredient[]>();
 
   getIngredients() {
     return this.ingredients.slice();
+  }
+
+  getIngredient(index: number) {
+    return this.ingredients[index];
   }
 
   addIngredient(ingredient: Ingredient) {
@@ -23,6 +27,16 @@ ingredientsChanged = new Subject<Ingredient[]>();
     //   this.addIngredient(ingredient);
     // }
     this.ingredients.push(...ingredients);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  updateIngredient( index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
